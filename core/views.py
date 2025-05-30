264dbed9efaae4714ba3d4da6e59c61b0abf91ad
Greg <sogmeadmug@gmail.com>
2025-05-30 04:15:24 +0300

    hw_21:  документация вьюшек, пруфы для задания

--- core/views.py
+++ core/views.py
@@ -4,6 +4,11 @@ from core.data import *
 # Create your views here.
 
 def landing(request):
+    """
+    Представление для главной страницы.
+    :param request: запрос
+    :returns render: Рендер главной страницы
+    """
     context = {
         'masters': masters,
         'services': services,
@@ -11,6 +16,11 @@ def landing(request):
     return render(request, "landing.html", context)
 
 def thanks(request):
+    """
+    Представление, вызываемое при нажатии на кнопку 'Записаться'.
+    :param request: запрос
+    :returns render: Рендер станицы с ответом
+    """
     context = {
     'masters': masters,
     'services': services,
@@ -18,12 +28,23 @@ def thanks(request):
     return render(request, 'thanks.html', context)
 
 def orders_list(request):
+    """
+    Представление для просмотра всех записей.
+    :param request: запрос
+    :returns render: Рендер главной страницы, модифицированный для показа всех записей.
+    """
     context = {
         "orders" : orders
     }
     return render(request, "orders.html", context)
 
-def order_detail(request, order_id):
+def order_detail(request, order_id: int):
+    """
+    Представление для одной записи.
+    :param request: запрос
+    :param order_id: номер записи в базе данных
+    :returns render: Рендер главной страницы, модифицированный для показа данных о конкретной записи
+    """
     order = [order for order in orders if order['id']== order_id][0]
     context = {
         "order": order
@@ -31,6 +52,11 @@ def order_detail(request, order_id):
     return render(request, 'orders_detail.html', context)
 
 def masters_list(request):
+    """
+    Представление для списка всех мастеров.
+    :param request: запрос
+    :returns render: Рендер главной страницы, модифицированный для показа списка всех мастеров с их фото.
+    """
     context = {
         "masters": masters
     }
